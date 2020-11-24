027ba1c0c2b78c82239fddd36aad315e2516d840
phcloud1devops <59385067+Cloud1PH@users.noreply.github.com>
2020-11-24 23:26:32 +0800

    Update appsecnodejsdeploy.sh

--- appsecnodejsdeploy.sh
+++ appsecnodejsdeploy.sh
@@ -17,8 +17,8 @@
           npm install --save trend_app_protect
           cat <<EOF >trend_app_protect.json
           {
-          "key":"${AccessKey}",
-          "secret":"${SecretKey}"
+          "key":"d2baeea9-9d70-4552-b527-9e300738e9fe",
+          "secret":"90039018-7866-4c96-8a60-6000a5f9ec58"
           }
           EOF
           sed -i "2 a require('trend_app_protect');" server.js
